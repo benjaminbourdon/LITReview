e97d5014a84003bd4d88d5318eba42dada430d61
Benjamin B <dev@benjamin-b.fr>
2023-05-24 01:16:06 +0200

    Change verbose name of fields

--- review/models.py
+++ review/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class Ticket(models.Model):
-    title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2048, blank=True)
+    title = models.CharField(max_length=128, verbose_name="titre")
+    description = models.TextField(
+        max_length=2048, blank=True, verbose_name="description"
+    )
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, verbose_name="image")
     time_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
@@ -29,10 +31,11 @@ class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)]
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        verbose_name="note",
     )
-    headline = models.CharField(max_length=128)
-    body = models.CharField(max_length=8192, blank=True)
+    headline = models.CharField(max_length=128, verbose_name="titre")
+    body = models.CharField(max_length=8192, blank=True, verbose_name="commentaire")
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
 
