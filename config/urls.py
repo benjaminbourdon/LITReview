1af3338a36ab235b25b4d5ea96dc9e67678171c2
Benjamin B <dev@benjamin-b.fr>
2023-05-29 11:08:06 +0200

    Add a redirect when accessing root url

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 import core_views
 
 urlpatterns = [
+    path("", core_views.home),
     path("admin/", admin.site.urls),
     path("review/", include("review.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
--- core_views.py
+++ core_views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic.edit import CreateView
 
 
@@ -6,3 +8,10 @@ class Signup(CreateView):
     form_class = UserCreationForm
     template_name = "registration/signup.html"
     success_url = "inscription"
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("my_posts"))
+    else:
+        return HttpResponseRedirect(reverse("login"))
